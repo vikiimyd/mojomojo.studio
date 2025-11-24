8a8a5174155340afdb5d8be3dbddbe4de9ce1545
vikiimyd <vikiimyd@gmail.com>
2025-11-24 09:25:58 -0500

    Add files via upload

---
+++ interactivity.js
@@ -0,0 +1,581 @@
+// Modal //
+// Fabric Library //
+
+// Fabric 1 //
+
+$("#fabric1").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").removeClass("hidden");
+  $("#modalfabric").removeClass("hidden");
+  $("#fabric1-section").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+
+$("#button-close-modal-fabric").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").addClass("hidden");
+  $("#modalfabric").addClass("hidden");
+  $("#fabric1-section").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+// Fabric 2 //
+
+$("#fabric2").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").removeClass("hidden");
+  $("#modalfabric").removeClass("hidden");
+  $("#fabric2-section").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+
+$("#button-close-modal-fabric").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").addClass("hidden");
+  $("#fabric2-section").addClass("hidden");
+  $("#modalfabric").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+// Fabric 3 //
+
+$("#fabric3").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").removeClass("hidden");
+  $("#modalfabric").removeClass("hidden");
+  $("#fabric3-section").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+
+$("#button-close-modal-fabric").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").addClass("hidden");
+  $("#fabric3-section").addClass("hidden");
+  $("#modalfabric").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+// Fabric 4 //
+
+$("#fabric4").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").removeClass("hidden");
+  $("#modalfabric").removeClass("hidden");
+  $("#fabric4-section").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+
+$("#button-close-modal-fabric").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").addClass("hidden");
+  $("#fabric4-section").addClass("hidden");
+  $("#modalfabric").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+// Fabric 5 //
+
+$("#fabric5").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").removeClass("hidden");
+  $("#modalfabric").removeClass("hidden");
+  $("#fabric5-section").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+
+$("#button-close-modal-fabric").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").addClass("hidden");
+  $("#fabric5-section").addClass("hidden");
+  $("#modalfabric").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+// Fabric 6 //
+
+$("#fabric6").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").removeClass("hidden");
+  $("#modalfabric").removeClass("hidden");
+  $("#fabric6-section").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+
+$("#button-close-modal-fabric").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").addClass("hidden");
+  $("#fabric6-section").addClass("hidden");
+  $("#modalfabric").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+// Shop //
+// Product 1 //
+
+$("#unit1").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalshopbg").removeClass("hidden");
+  $("#modalshop").removeClass("hidden");
+  $("#unit1content").removeClass("hidden");
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#button-close-modal-shop").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalshopbg").addClass("hidden");
+  $("#modalshop").addClass("hidden");
+  $("#unit1content").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#fabric1id").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").removeClass("hidden");
+  $("#modalfabric").removeClass("hidden");
+  $("#fabric1-section").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+// Product 2 //
+
+$("#unit2").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalshopbg").removeClass("hidden");
+  $("#unit2content").removeClass("hidden");
+  $("#modalshop").removeClass("hidden");
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#button-close-modal-shop").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalshopbg").addClass("hidden");
+  $("#unit2content").addClass("hidden");
+  $("#modalshop").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#fabric2id").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").removeClass("hidden");
+  $("#fabric2-section").removeClass("hidden");
+  $("#modalfabric").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+// Product 3 //
+
+$("#unit3").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalshopbg").removeClass("hidden");
+  $("#unit3content").removeClass("hidden");
+  $("#modalshop").removeClass("hidden");
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#button-close-modal-shop").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalshopbg").addClass("hidden");
+  $("#unit3content").addClass("hidden");
+  $("#modalshop").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#fabric3id").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").removeClass("hidden");
+  $("#fabric3-section").removeClass("hidden");
+  $("#modalfabric").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+// Product 4 //
+
+$("#unit4").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalshopbg").removeClass("hidden");
+  $("#unit4content").removeClass("hidden");
+  $("#modalshop").removeClass("hidden");
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#button-close-modal-shop").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalshopbg").addClass("hidden");
+  $("#unit4content").addClass("hidden");
+  $("#modalshop").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#fabric4id").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").removeClass("hidden");
+  $("#fabric4-section").removeClass("hidden");
+  $("#modalfabric").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+// Product 5 //
+
+$("#unit5").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalshopbg").removeClass("hidden");
+  $("#unit5content").removeClass("hidden");
+  $("#modalshop").removeClass("hidden");
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#button-close-modal-shop").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalshopbg").addClass("hidden");
+  $("#unit5content").addClass("hidden");
+  $("#modalshop").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#fabric5id").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").removeClass("hidden");
+  $("#fabric5-section").removeClass("hidden");
+  $("#modalfabric").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+// Product 6 //
+
+$("#unit6").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalshopbg").removeClass("hidden");
+  $("#unit6content").removeClass("hidden");
+  $("#modalshop").removeClass("hidden");
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#button-close-modal-shop").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalshopbg").addClass("hidden");
+  $("#unit6content").addClass("hidden");
+  $("#modalshop").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#fabric6id").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#modalfabricbg").removeClass("hidden");
+  $("#fabric6-section").removeClass("hidden");
+  $("#modalfabric").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+// Menu //
+
+$("#button-menu").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  if ($("#menubg").hasClass("hidden")) { // ← element has css class
+
+    // ↓ action snippets (class on element) ↓
+    $("#menubg").removeClass("hidden");
+    $("#menu").removeClass("hidden");
+
+
+    // ↑ action snippets (class on element) ↑
+
+  } else { // ← element has css class
+
+    // ↓ action snippets (class not on element) ↓
+    $("#menubg").addClass("hidden");
+    $("#menu").addClass("hidden");
+
+
+    // ↑ action snippets (class not on element) ↑
+
+  } // ← element has css class
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+
+$(window).on("resize", function () { // ← resize browser window event
+
+  // ↓ action snippets ↓
+  if (window.matchMedia("(max-width: 770px)").matches) { // ← get browser width
+
+    // ↓ action snippets (< width) ↓
+    $("#left-menu").addClass("hidden");
+    $("#right-menu").addClass("hidden");
+    $("#button-menu").removeClass("hidden");
+    $("#shopicon").removeClass("hidden");
+
+
+    // ↑ action snippets (< width) ↑
+
+  } else { // ← get browser width
+
+    // ↓ action snippets (> width) ↓
+    $("#left-menu").removeClass("hidden");
+    $("#right-menu").removeClass("hidden");
+    $("#button-menu").addClass("hidden");
+    $("#shopicon").addClass("hidden");
+    $("#menubg").addClass("hidden");
+    $("#menu").addClass("hidden");
+
+    // ↑ action snippets (> width) ↑
+
+  } // ← get browser width
+
+  // ↑ action snippets ↑
+
+}); // ← resize browser window event
+
+
+$(document).ready(function () { // ← page ready event
+
+  // ↓ action snippets ↓
+  if (window.matchMedia("(max-width: 770px)").matches) { // ← get browser width
+
+    // ↓ action snippets (< width) ↓
+    $("#left-menu").addClass("hidden");
+    $("#right-menu").addClass("hidden");
+    $("#button-menu").removeClass("hidden");
+    $("#shopicon").removeClass("hidden");
+
+    // ↑ action snippets (< width) ↑
+
+  } else { // ← get browser width
+
+    // ↓ action snippets (> width) ↓
+    $("#left-menu").removeClass("hidden");
+    $("#right-menu").removeClass("hidden");
+    $("#button-menu").addClass("hidden");
+    $("#shopicon").addClass("hidden");
+    $("#menubg").addClass("hidden");
+    $("#menu").addClass("hidden");
+
+    // ↑ action snippets (> width) ↑
+
+  } // ← get browser width
+
+  // ↑ action snippets ↑
+
+}); // ← page ready event
+
+// Fabric Library Gallery //
+$(document).ready(function () { // ← page ready event
+
+  // ↓ action snippets ↓
+  $("#fl1").removeClass("hidden");
+  $("#fl2").removeClass("hidden");
+  $("#fl3").removeClass("hidden");
+  $("#fl4").removeClass("hidden");
+  $("#fl5").removeClass("hidden");
+  $("#fl6").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← page ready event
+
+
+$("#allfabric").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#fl1").removeClass("hidden");
+  $("#fl2").removeClass("hidden");
+  $("#fl3").removeClass("hidden");
+  $("#fl4").removeClass("hidden");
+  $("#fl5").removeClass("hidden");
+  $("#fl6").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#calmfabric").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#fl1, #fl3").removeClass("hidden");
+  $("#fl2, #fl4, #fl5, #fl6").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#resilientfabric").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#fl2, #fl5").removeClass("hidden");
+  $("#fl1, #fl3, #fl4, #fl6").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#serenefabric").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#fl4, #fl6").removeClass("hidden");
+  $("#fl2, #fl1, #fl5, #fl3").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+// Shop Gallery //
+$(document).ready(function () { // ← page ready event
+
+  // ↓ action snippets ↓
+  $("#s1").removeClass("hidden");
+  $("#s2").removeClass("hidden");
+  $("#s3").removeClass("hidden");
+  $("#s4").removeClass("hidden");
+  $("#s5").removeClass("hidden");
+  $("#s6").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← page ready event
+
+
+$("#allproducts").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#s1").removeClass("hidden");
+  $("#s2").removeClass("hidden");
+  $("#s3").removeClass("hidden");
+  $("#s4").removeClass("hidden");
+  $("#s5").removeClass("hidden");
+  $("#s6").removeClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#calmproducts").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#s1, #s3").removeClass("hidden");
+  $("#s2, #s4, #s5, #s6").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#resilientproducts").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#s2, #s5").removeClass("hidden");
+  $("#s1, #s3, #s4, #s6").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+$("#sereneproducts").on("click", function () { // ← click event
+
+  // ↓ action snippets ↓
+  $("#s4, #s6").removeClass("hidden");
+  $("#s2, #s1, #s5, #s3").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
+
+
+$('.tags button').on('click', function () {
+  $('.tags button').removeClass('active-tag');
+  $(this).addClass('active-tag');
+});
+
+$(document).ready(function () {
+  $('#allproducts, #allfabric').addClass('active-tag');
+});
